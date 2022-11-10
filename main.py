db5ed110c4549078f43066bdee011dfff74f98ae
Olga <o.panova27@gmail.com>
2022-11-10 21:26:42 +0300

    Add case if there is no book

--- main.py
+++ main.py
@@ -1,21 +1,31 @@
 import os
 import requests
 
+
+def check_for_redirect(response):
+    if response.status_code == 302:
+        raise requests.HTTPError()
+
+
 url = "https://tululu.org/txt.php"
 
 book_id = 1
 
 while book_id <= 10:
-    params = {
-        "id": book_id
-    }
-    response = requests.get(url, params=params)
-    response.raise_for_status() 
+    try:
+        params = {
+            "id": book_id
+        }
+        response = requests.get(url, params=params, allow_redirects=False)
+        response.raise_for_status() 
+        check_for_redirect(response)
+        
+        os.makedirs("books", exist_ok=True)
+        filename = f'books/id{book_id}.txt'
+        with open(filename, 'w') as file:
+            file.write(response.text)
+        
+    except requests.HTTPError:
+        pass
     
-    os.makedirs("books", exist_ok=True)
-    filename = f'books/id{book_id}.txt'
-    with open(filename, 'w') as file:
-        file.write(response.text)
     book_id += 1
-    
-    
