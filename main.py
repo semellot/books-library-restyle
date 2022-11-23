14eabbc6fa1ac55c8a1677a3195d3bdbf7234b9f
Olga <o.panova27@gmail.com>
2022-11-23 22:59:17 +0300

    Use params in requests.get

--- main.py
+++ main.py
@@ -13,8 +13,11 @@ def check_for_redirect(response):
         raise requests.HTTPError()
 
 
-def download_txt(url, filename, folder='books/'):
-    response = requests.get(url, allow_redirects=False)
+def download_txt(url, filename, book_id, folder='books/'):
+    params = {
+        'id': book_id
+    }
+    response = requests.get(url, params=params, allow_redirects=False)
     response.raise_for_status()
     check_for_redirect(response)
     
@@ -84,8 +87,8 @@ if __name__ == "__main__":
             book = parse_book_page(response.text, url)
             
             filename = f'{book_id}. {book["title"]}.txt'
-            url = f'https://tululu.org/txt.php?id={book_id}'
-            download_txt(url, filename)
+            url = 'https://tululu.org/txt.php'
+            download_txt(url, filename, book_id)
             
             download_image(book['image_url'], book['image_name'])
 
