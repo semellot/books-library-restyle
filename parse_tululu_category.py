10d7f03e86ba3da3fa9fd109d1862752f94393ba
Olga <o.panova27@gmail.com>
2022-12-01 00:28:41 +0300

    Print books url for 10 pages

--- parse_tululu_category.py
+++ parse_tululu_category.py
@@ -4,14 +4,15 @@ from urllib.parse import urljoin
 
 
 if __name__ == "__main__":
-    url = 'https://tululu.org/l55/'
-    response = requests.get(url)
-    response.raise_for_status()
-    
-    soup = BeautifulSoup(response.text, 'lxml')
-    
-    books = soup.find_all('div', class_='bookimage')
-    for book in books:
-        book_href = book.find('a')['href']
-        book_url = urljoin('https://tululu.org', book_href)
-        print(book_url)
+    for page in range(1,11):
+        url = f'https://tululu.org/l55/{page}'
+        response = requests.get(url)
+        response.raise_for_status()
+        
+        soup = BeautifulSoup(response.text, 'lxml')
+        
+        books = soup.find_all('div', class_='bookimage')
+        for book in books:
+            book_href = book.find('a')['href']
+            book_url = urljoin('https://tululu.org', book_href)
+            print(book_url)
