7c65422e6dc20b16ad1e4d18b67a83f97a72ee76
Olga <o.panova27@gmail.com>
2022-12-23 12:59:05 +0300

    Add additional redirect check

--- parse_tululu_category.py
+++ parse_tululu_category.py
@@ -27,6 +27,7 @@ if __name__ == '__main__':
             url = f'https://tululu.org/l55/{page}'
             response = requests.get(url)
             response.raise_for_status()
+            check_for_redirect(response)
             
             soup = BeautifulSoup(response.text, 'lxml')
             selector = 'div.bookimage'
